8e8783a40672c7d474b530c832d6518014f80d45
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-05 18:03:00 -0500

    add more dotfiles

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+echo "sup dude"
+neofetch
+
+# aplication aliases
+alias code 'codium'
+
+# config dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
---
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -0,0 +1,45 @@
+require("mason").setup()
+require("mason-lspconfig").setup({
+	ensure_installed = { "cssls", "cssmodules_ls", "denols", "astro", "bashls", "emmet_ls", "graphql", "html", "jsonls", "quick_lint_js", "tsserver", "sumneko_lua", "marksman", "intelephense", "rust_analyzer", "sqlls", "svelte", "tailwindcss", "volar", "lemminx", "yamlls" }
+})
+
+local on_attach = function(_, _)
+	vim.keymap.set ('n','<leader>rn', vim.lsp.buf .rename, {})
+vim.keymap.set ('n','<leader>ca', vim. lsp.buf .code_action, {})
+
+vim.keymap.set ('n','gd', vim.lsp.buf.definition, {})
+vim.keymap.set('n','gi', vim.lsp.buf. implementation, {})
+vim.keymap. set('n', 'gr', require ('telescope.builtin').lsp_references, {})
+vim.keymap.set ('n', 'K', vim.lsp.buf. hover, {})
+end
+
+require("lspconfig").emmet_ls.setup{
+	on_attach = on_attach
+}
+require("lspconfig").tsserver.setup{
+	on_attach = on_attach
+}
+require("lspconfig").jsonls.setup{
+	on_attach = on_attach
+}
+require("lspconfig").volar.setup{
+	on_attach = on_attach
+}
+require("lspconfig").cssls.setup{
+	on_attach = on_attach
+}
+require("lspconfig").cssmodules_ls.setup{
+	on_attach = on_attach
+}
+require("lspconfig").tailwindcss.setup{
+	on_attach = on_attach
+}
+require("lspconfig").html.setup{
+	on_attach = on_attach
+}
+require("lspconfig").jsonls.setup{
+	on_attach = on_attach
+}
+require("lspconfig").sumneko_lua.setup{
+	on_attach = on_attach
+}
