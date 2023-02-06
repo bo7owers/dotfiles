7c4a4a95b3425edfc4c586d9a42267df21e2e66c
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-05 19:29:08 -0500

    add auto install (packages) script

--- install.sh
+++ install.sh
@@ -4,3 +4,14 @@
 # https://nixos.org/download.html#nix-install-linux
 sh <(curl -L https://nixos.org/nix/install) --daemon
 
+# Automatically install packages
+
+nix-env -iA \
+	nixpkgs.tmux \
+	nixpkgs.yarn \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.direnv
+
+# add more!
+# https://search.nixos.org/packages
