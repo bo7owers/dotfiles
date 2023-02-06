c7bec5f893c22dd50a6121ae81a6c6fdcb2b12bd
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-05 19:21:38 -0500

    create packages script

---
+++ packages.sh
@@ -0,0 +1,12 @@
+#! bin/bash 
+# Automatically install packages
+
+nix-env -iA \
+	nixpkgs.tmux \
+	nixpkgs.yarn \
+	nixpkgs.ripgrep \
+	nixpkgs.bat \
+	nixpkgs.direnv
+
+# add more!
+# https://search.nixos.org/packages
