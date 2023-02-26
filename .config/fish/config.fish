8ac5ccb184ce573572f801bf2e74d188932ddd90
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-02-26 15:25:42 -0500

    add git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-echo "sup dude"
 neofetch
 
 # aplication aliases
@@ -6,3 +5,15 @@ alias code 'codium'
 
 # config dotfiles
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+# git aliases
+alias gs='git stage'
+alias gsa='git stage .'
+alias gc='git commit -m'
+alias gch='git checkout'
+alias gpsh='git push'
+alias gpll='git pull'
+alias gm='git merge'
+alias gd='git branch -D'
+alias gb='git branch'
+alias gstt='git status'
