48d2660214cd64906be0c1b3e2b01ade619dfe77
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-03-18 12:43:22 -0400

    Test args in gc

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 # git aliases
 alias gs='git stage'
 alias gsa='git stage .'
-alias gc='git commit -m'
+alias gc="git commit -m "
 alias gch='git checkout'
 alias gpsh='git push'
 alias gpll='git pull'
