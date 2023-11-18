34a31899ee5dd3b6490dc12f25010788059b1111
bo7owers <74225602+bo7owers@users.noreply.github.com>
2023-11-18 08:29:23 -0500

    set up base secondary screen bar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -8,4 +8,8 @@ killall -q polybar
 # Launch Polybar, using default config location ~/.config/polybar/config.ini
 # polybar left 2>&1 | tee -a /tmp/polybar.log & disown
 polybar left &
+polybar right &
+## if [[ $(xrandr -q | grep 'DP-1 connected')]]; then
+## 	polybar right &
+##fi
 # echo "Polybar launched..."
